c5c408bc54c27f0c66829b516619ce6886be9135
Jairo Ushiña <jago86@gmail.com>
2022-12-27 09:43:38 -0500

    M1. E5.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return Auth::user();
+    // return $request->user();
 });
