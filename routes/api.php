cd3f64c0c6a867e89c423988ebabd745a4c02799
Jairo Ushiña <jago86@gmail.com>
2023-01-04 20:28:38 -0500

    M1. E9.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
+Route::middleware('auth:sanctum')->post('orders', function () {
+    abort_unless(Auth::user()->tokenCan('orders:create'), 403, "You don't have permissions to perform this action.");
+
+    return [
+        'message' => 'Order created',
+    ];
+});
+
 Route::post('login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
